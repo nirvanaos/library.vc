a30f1c5de570893a6ee7db9770c778f5d32025a3
Igor Popov <silver.popov@gmail.com>
2019-04-18 14:53:20 +0300

    Build script.

---
+++ build.ps1
@@ -0,0 +1,8 @@
+﻿$solution = "Library.sln"
+nuget restore "$solution" -Source "\\NAS\Home\NuGet"
+&"$Env:VS2017INSTALLDIR\Common7\IDE\devenv.com" "$solution" /build "Debug|x86"
+&"$Env:VS2017INSTALLDIR\Common7\IDE\devenv.com" "$solution" /build "Release|x86"
+&"$Env:VS2017INSTALLDIR\Common7\IDE\devenv.com" "$solution" /build "Debug|x64"
+&"$Env:VS2017INSTALLDIR\Common7\IDE\devenv.com" "$solution" /build "Release|x64"
+Write-NuGetPackage .\CoApp\Library.autopkg
+Move-Item -Path *.nupkg -Destination \\NAS\Home\NuGet -Force
