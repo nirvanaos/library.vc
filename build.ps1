4cbf5946480ef90304a6869e0bb811a547e47441
Igor Popov <popov@intromarin.ru>
2020-01-28 07:26:40 +0300

    Build script.

--- build.ps1
+++ build.ps1
@@ -1,9 +1,9 @@
 ﻿$solution = "Library.sln"
 nuget restore "$solution" -Source "\\NAS\Home\NuGet"
-&"$Env:VS2017INSTALLDIR\Common7\IDE\devenv.com" "$solution" /build "Debug|x86"
-&"$Env:VS2017INSTALLDIR\Common7\IDE\devenv.com" "$solution" /build "Release|x86"
-&"$Env:VS2017INSTALLDIR\Common7\IDE\devenv.com" "$solution" /build "Debug|x64"
-&"$Env:VS2017INSTALLDIR\Common7\IDE\devenv.com" "$solution" /build "Release|x64"
+&"$Env:VS2019INSTALLDIR\Common7\IDE\devenv.com" "$solution" /build "Debug|x86"
+&"$Env:VS2019INSTALLDIR\Common7\IDE\devenv.com" "$solution" /build "Release|x86"
+&"$Env:VS2019INSTALLDIR\Common7\IDE\devenv.com" "$solution" /build "Debug|x64"
+&"$Env:VS2019INSTALLDIR\Common7\IDE\devenv.com" "$solution" /build "Release|x64"
 Write-NuGetPackage .\CoApp\Library.autopkg
 Write-NuGetPackage .\CoApp\googletest.autopkg
 Write-NuGetPackage .\CoApp\LibMock.autopkg
