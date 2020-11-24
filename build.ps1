6274ec492cbff5b2378edfc4cbcf0f8b59475cbd
silver-popov <silver.popov@gmail.com>
2020-11-24 10:48:15 +0300

    SRC

--- build.ps1
+++ build.ps1
@@ -1,9 +1,9 @@
 ﻿$solution = "Library.sln"
 nuget restore "$solution" -Source "\\NAS\Home\NuGet"
-&"$Env:VS2019INSTALLDIR\Common7\IDE\devenv.com" "$solution" /build "Debug|x86"
-&"$Env:VS2019INSTALLDIR\Common7\IDE\devenv.com" "$solution" /build "Release|x86"
-&"$Env:VS2019INSTALLDIR\Common7\IDE\devenv.com" "$solution" /build "Debug|x64"
-&"$Env:VS2019INSTALLDIR\Common7\IDE\devenv.com" "$solution" /build "Release|x64"
+&"$Env:VSINSTALLDIR\Common7\IDE\devenv.com" "$solution" /build "Debug|x86"
+&"$Env:VSINSTALLDIR\Common7\IDE\devenv.com" "$solution" /build "Release|x86"
+&"$Env:VSINSTALLDIR\Common7\IDE\devenv.com" "$solution" /build "Debug|x64"
+&"$Env:VSINSTALLDIR\Common7\IDE\devenv.com" "$solution" /build "Release|x64"
 Write-NuGetPackage .\CoApp\Library.vc.autopkg
 Write-NuGetPackage .\CoApp\googletest.vc.autopkg
 Write-NuGetPackage .\CoApp\LibMock.vc.autopkg
